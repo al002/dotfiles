b7099a3b6b162b5fd5562d5417c91aef921f3989
al002 <al002@disroot.org>
2025-12-25 12:11:11 +0800

    chore: nvim lsp config change

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,82 +1,85 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
 local servers = {
-  html = {},
-  cssls = {},
-  clangd = {
-    cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
-  },
-  jsonls = {},
-  ts_ls = {},
-  eslint = {},
-  rust_analyzer = {
-    settings = {
-      ["rust-analyzer"] = {
-        files = {
-          excludeDirs = { "target", "node_modules" },
-        },
-      },
-    },
-  },
-  tailwindcss = {},
-  elixirls = {
-    cmd = {
-      "/home/al/.local/share/nvim/mason/packages/elixir-ls/language_server.sh",
-    },
-  },
-  gopls = {},
-  solidity_ls = {},
-  lemminx = {},
-  pyright = {},
-  ruff = {
-    trace = "messages",
-    init_options = {
-      settings = {
-        fixAll = true,
-        organizeImports = false, -- let isort take care of organizeImports
-        -- extra CLI arguments
-        -- https://docs.astral.sh/ruff/configuration/#command-line-interface
-        -- https://docs.astral.sh/ruff/rules/
-        args = {
-          "--preview", -- Use experimental features
-          "--ignore",
-          table.concat({
-            "E111", -- indentation-with-invalid-multiple
-            "E114", -- indentation-with-invalid-multiple-comment
-            "E402", -- module-import-not-at-top-of-file
-            "E501", -- line-too-long
-            "E702", -- multiple-statements-on-one-line-semicolon
-            "E731", -- lambda-assignment
-            "F401", -- unused-import  (note: should be handled by pyright as 'hint')
-          }, ","),
-        },
-        logLevel = "info",
-      },
-    },
-  },
-  csharp_ls = {
-    AutomaticWorkspaceInit = true,
-  },
+  "html",
+  "cssls",
+  "clangd",
+  "jsonls",
+  "ts_ls",
+  "eslint",
+  "rust_analyzer",
+  "tailwindcss",
+  "expert",
+  "gopls",
+  "solidity_ls",
+  "lemminx",
+  "pyright",
+  "ruff",
+  -- "csharp_ls",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
 
-for name, opts in pairs(servers) do
-  opts.on_init = nvlsp.on_init
-  opts.on_attach = nvlsp.on_attach
-  opts.capabilities = nvlsp.capabilities
+vim.lsp.enable(servers)
 
-  require("lspconfig")[name].setup(opts)
-end
+vim.lsp.config("clangd", {
+  cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+})
 
-require("csharpls_extended").buf_read_cmd_bind()
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      files = {
+        excludeDirs = { "target", "node_modules" },
+      },
+    },
+  },
+})
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+vim.lsp.config("pyright", {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "strict",
+        diagnosticMode = "workspace",
+        inlayHints = {
+          variableTypes = true,
+          functionReturnTypes = true,
+        },
+      },
+    },
+  },
+})
+
+vim.lsp.config("ruff", {
+  trace = "messages",
+  init_options = {
+    settings = {
+      fixAll = true,
+      organizeImports = false, -- let isort take care of organizeImports
+      -- extra CLI arguments
+      -- https://docs.astral.sh/ruff/configuration/#command-line-interface
+      -- https://docs.astral.sh/ruff/rules/
+      args = {
+        "--preview", -- Use experimental features
+        "--ignore",
+        table.concat({
+          "E111", -- indentation-with-invalid-multiple
+          "E114", -- indentation-with-invalid-multiple-comment
+          "E402", -- module-import-not-at-top-of-file
+          "E501", -- line-too-long
+          "E702", -- multiple-statements-on-one-line-semicolon
+          "E731", -- lambda-assignment
+          "F401", -- unused-import  (note: should be handled by pyright as 'hint')
+        }, ","),
+      },
+      logLevel = "info",
+    },
+  },
+})
+
+-- vim.lsp.config("csharp_ls", {
+--   AutomaticWorkspaceInit = true,
+-- })
+
+-- require("csharpls_extended").buf_read_cmd_bind()
+
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -14,7 +14,7 @@ return {
   },
 
   {
-    "williamboman/mason-lspconfig.nvim",
+  	"mason-org/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
         -- lua
@@ -29,7 +29,7 @@ return {
         "tailwindcss-language-server",
         "deno",
         "json-lsp",
-        "elixir-ls",
+        "expert",
         "gopls",
         "sqls",
         "csharp-language-server",
@@ -91,135 +91,6 @@ return {
   },
 
   {
-    "augmentcode/augment.vim",
-    lazy = false,
-  },
-
-  {
     "Decodetalkers/csharpls-extended-lsp.nvim",
   },
-
-  -- {
-  --   "milanglacier/minuet-ai.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("minuet").setup {
-  --       provider = "openai_compatible",
-  --       provider_options = {
-  --         openai_compatible = {
-  --           -- model = "google/gemini-2.5-pro-exp-03-25:free",
-  --           model = "deepseek/deepseek-chat-v3-0324",
-  --           system = require("minuet.config").default_system,
-  --           few_shots = require("minuet.config").default_few_shots,
-  --           chat_input = require("minuet.config").default_chat_input,
-  --           stream = true,
-  --           end_point = "https://openrouter.ai/api/v1/chat/completions",
-  --           api_key = "OPENROUTER_API_KEY",
-  --           name = "openrouter",
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
-  --
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   opts = function(_, opts)
-  --     -- if you wish to use autocomplete
-  --     table.insert(opts.sources, 1, {
-  --       name = "minuet",
-  --       group_index = 1,
-  --       priority = 100,
-  --     })
-  --
-  --     opts.performance = {
-  --       -- It is recommended to increase the timeout duration due to
-  --       -- the typically slower response speed of LLMs compared to
-  --       -- other completion sources. This is not needed when you only
-  --       -- need manual completion.
-  --       fetching_timeout = 2000,
-  --     }
-  --
-  --     opts.mapping = vim.tbl_deep_extend("force", opts.mapping or {}, {
-  --       -- if you wish to use manual complete
-  --       ["<A-y>"] = require("minuet").make_cmp_map(),
-  --     })
-  --   end,
-  -- },
-
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   version = false,
-  --   debug = true,
-  --   opts = {
-  --     provider = "openrouter",
-  --     -- openrouter = {
-  --     --   -- model = "google/gemini-2.5-pro-exp-03-25:free",
-  --     --   model = "deepseek/deepseek-chat-v3-0324",
-  --     --   timeout = 3000,
-  --     --   temperature = 1,
-  --     --   -- max_tokens = 1000000,
-  --     --   max_tokens = 64000,
-  --     -- },
-  --     vendors = {
-  --       ["openrouter"] = {
-  --         __inherited_from = "openai",
-  --         endpoint = "https://openrouter.ai/api/v1",
-  --         api_key_name = "OPENROUTER_API_KEY",
-  --         model = "google/gemini-2.5-pro-exp-03-25:free",
-  --         -- model = "deepseek/deepseek-chat-v3-0324"
-  --       },
-  --     },
-  --   },
-  --   build = "make",
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "stevearc/dressing.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     --- The below dependencies are optional,
-  --     -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
-  --     -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-  --     -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-  --     -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
-  --     -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --     -- {
-  --     --   -- support for image pasting
-  --     --   "HakonHarnes/img-clip.nvim",
-  --     --   event = "VeryLazy",
-  --     --   opts = {
-  --     --     -- recommended settings
-  --     --     default = {
-  --     --       embed_image_as_base64 = false,
-  --     --       prompt_for_file_name = false,
-  --     --       drag_and_drop = {
-  --     --         insert_mode = true,
-  --     --       },
-  --     --       -- required for Windows users
-  --     --       use_absolute_path = true,
-  --     --     },
-  --     --   },
-  --     -- },
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  -- },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
 }
