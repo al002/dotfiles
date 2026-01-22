3b4df79ee28555a956d14d275c30f74e821e5285
al002 <al002@disroot.org>
2026-01-22 11:52:54 +0800

    shortcut to show file absolute path

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,3 +9,27 @@ map("i", "jk", "<ESC>")
 
 map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "Floating LSP diagnostic" })
 map("n", "<leader>tt", require("base46").toggle_transparency, { desc = "Toggle transparency" })
+
+-- Show file absolute path
+map("n", "<leader>pf", function()
+  local path = vim.fn.expand "%:p"
+  if path == "" then
+    vim.notify("Path not exists", vim.log.levels.WARN)
+    return
+  end
+
+  vim.notify(path)
+end, { desc = "Show file absolute path" })
+
+-- Copy file absolute path
+map("n", "<leader>pc", function()
+  local path = vim.fn.expand "%:p"
+  if path == "" then
+    vim.notify("Path not exists", vim.log.levels.WARN)
+    return
+  end
+
+  vim.fn.setreg("+", path)
+  vim.fn.setreg("*", path)
+  vim.notify("Path " .. path .. " copied")
+end, { desc = "Show file absolute path" })
